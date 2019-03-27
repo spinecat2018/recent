ed0a07c86a355b2c5dc90af7b1758d7e0f77bb5d
spinecat <157171828@qq.com>
2019-03-27 18:17:47 +0800

    创建了数据库，含3个表

--- src/com/nightmare/recent/AddPoint.java
+++ src/com/nightmare/recent/AddPoint.java
@@ -1,10 +1,16 @@
 package com.nightmare.recent;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -17,20 +23,32 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 
+
 public class AddPoint extends Activity {
 
-	String colorCode,description;
+	List<String> colorRange = Arrays.asList(
+			"#925649",
+			"#aa24a8",
+			"#f62541");
 	
 	
 	
 	private List<Fruit> fruitList = new ArrayList<Fruit>();
 	
+	String colorCode,description;
+	
+	public static String colorTableCode;
+		
+	private ColorBase colorBaseHelper;
+	
+	 
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
-		
+		//Log.d("recent","create");	
 		
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -38,30 +56,19 @@ public class AddPoint extends Activity {
 		
 				
 		initFruits();
-		
-		//ArrayAdapter<String> adapter0 = new ArrayAdapter<String>(
-			//	AddPoint.this, android.R.layout.simple_spinner_item, data);
-				
-		final FruitAdapter adapter = new FruitAdapter(
-			AddPoint.this,R.layout.color, fruitList);
+						
+		final FruitAdapter adapter = new FruitAdapter(AddPoint.this,R.layout.color, fruitList);
 		
 		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
 		
 		spinner.setAdapter(adapter);
-		
-		
-		
-		
+								
 		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 			@Override
 		     public void onItemSelected(AdapterView<?> parent, View view,int position, long id) {
-		         //System.out.println(spinner==parent);//true
-		         //System.out.println(view);
-		         //String data = adapter.getItem(position);//从适配器中获取被选择的数据项
-		         //String data = list.get(position);//从集合中获取被选择的数据项
 				Fruit selectColor = adapter.getItem(position);
 				colorCode = selectColor.getName();
-		        
+				
 		     }
 
 		     @Override
@@ -70,54 +77,61 @@ public class AddPoint extends Activity {
 		     }
 
 		});
-
+		//创建数据库
+		
+		//Log.d("recent","before db");	
+		colorBaseHelper = new ColorBase(this, "colorDataBase.db", null, 1);
+		
+		final SQLiteDatabase colorBase= colorBaseHelper.getWritableDatabase();
+		
+		//Log.d("recent","after db");			
 		//保存按钮
+		
 		Button button1 = (Button) findViewById(R.id.buttonSave);
 		button1.setOnClickListener(new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-		//点击按钮，新建数据库，新建颜色表，添加数据	
-			
-		
-		EditText detail = (EditText) findViewById(R.id.edit_text1);
-		description = detail.getText().toString();
-		
-		Toast.makeText(AddPoint.this, colorCode +"+"+description ,
-		Toast.LENGTH_SHORT).show();
-		
-		
-		
-		}
+			@Override
+			public void onClick(View v) {
+			//点击按钮，添加数据	
+				int colorId	= colorRange.indexOf(colorCode);//获取颜色编码在数组中的序号
+				
+				EditText detail = (EditText) findViewById(R.id.edit_text1);
+				description = detail.getText().toString();
+				
+				ContentValues values = new ContentValues();
+				//  开始组装第一条数据
+				values.put("moment", System.currentTimeMillis()/1000+"");
+				values.put("description", description);
+				
+				colorBase.insert("c"+ colorId, null, values); //  插入第一条数据
+				
+				values.clear();
+				Log.d("recent","record"+System.currentTimeMillis()/1000+description);		
+				
+														
+				
+				
+				//Date moment = new Date();
+				
+				//Log.d("recent",System.currentTimeMillis()+" + "+ sdf.format(moment));	
+				//Toast.makeText(AddPoint.this, colorId +"+"+description ,
+				//Toast.LENGTH_SHORT).show();
+						
+			}
 		});
-		
-		
-		
 		
 		
 	}	
 	
 	private void initFruits() {
-		Fruit apple = new Fruit("#692fff");
+		Fruit apple = new Fruit(colorRange.get(0));
 		fruitList.add(apple);
-		Fruit banana = new Fruit("#004768");
+		Fruit banana = new Fruit(colorRange.get(1));
 		fruitList.add(banana);
-		Fruit orange = new Fruit("#007893");
+		Fruit orange = new Fruit(colorRange.get(2));
 		fruitList.add(orange);
-		Fruit watermelon = new Fruit("#00d946");
-		fruitList.add(watermelon);
-		Fruit pear = new Fruit("#00e67f");
-		fruitList.add(pear);
-		Fruit grape = new Fruit("#00a546");
-		fruitList.add(grape);
-		Fruit pineapple = new Fruit("#00c576");
-		fruitList.add(pineapple);
-		Fruit strawberry = new Fruit("#00b34a");
-		fruitList.add(strawberry);
-		Fruit cherry = new Fruit("#00d235");
-		fruitList.add(cherry);
-		Fruit mango = new Fruit("#006784");
-		fruitList.add(mango);
+		//Log.d("recent","init");	
 		}
+
 	
 	 
 	
---
+++ src/com/nightmare/recent/ColorBase.java
@@ -0,0 +1,54 @@
+package com.nightmare.recent;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+
+public class ColorBase extends SQLiteOpenHelper {
+	
+	
+	static String colorTableCode;
+	
+	private Context mContext;
+	
+	public ColorBase(Context context, String name, CursorFactory factory, int version) {
+			super(context, name, factory, version);
+			mContext = context;
+			Log.d("recent","construct");
+			}
+	
+	
+	@Override
+	public void onCreate(SQLiteDatabase colorBase) {
+		Log.d("recent","create");
+		
+		colorTableCodeGen("c0");
+		colorBase.execSQL(colorTableCode);
+		colorTableCodeGen("c1");
+		colorBase.execSQL(colorTableCode);
+		colorTableCodeGen("c2");
+		colorBase.execSQL(colorTableCode);
+		
+		Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+		
+
+	}
+
+	public  void colorTableCodeGen(String tableName){
+		
+		colorTableCode = "create table "+ tableName + " ("
+				+ "id integer primary key autoincrement, "
+				+ "moment text, "
+				+ "description text)";
+	}
+	
+}
